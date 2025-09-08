0924dc765648d586eca948a72a0e428c0685f35b
AhmedElabrass25 <ahmedelabrass2035@gmail.com>
2025-09-08 08:16:46 +0300

    Add env file & vercel.json

--- src/components/Auth/supabaseClient.jsx
+++ src/components/Auth/supabaseClient.jsx
@@ -1,7 +1,6 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
 
-let supabaseURL = 'https://ufmunskfeyijrmuqgbsg.supabase.co'
-  let supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVmbXVuc2tmZXlpanJtdXFnYnNnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYxODkwNzIsImV4cCI6MjA3MTc2NTA3Mn0.GdBYvjFBp3SgjNcjKbK1nEuT2JNqx8mQlud0Wm27mko'
-
-export const supabase = createClient(supabaseURL, supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseKey);
